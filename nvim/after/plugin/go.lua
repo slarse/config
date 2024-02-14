6020d158284081e93c7ff1e75ee766498e4be34d
Simon Larsén <slarse@slar.se>
2024-02-14 18:13:49 +0100

    Add missing go.lua module

---
+++ nvim/after/plugin/go.lua
@@ -0,0 +1,12 @@
+require("go").setup({
+	goimport = "gopls", -- if set to 'gopls' will use golsp format
+	gofmt = "gopls", -- if set to gopls will use golsp format
+	max_line_len = 120,
+	tag_transform = false,
+	test_dir = "",
+	comment_placeholder = "   ",
+	lsp_cfg = false,
+	lsp_gofumpt = false,
+	lsp_on_attach = false,
+	dap_debug = true,
+})
