089855b82a2f5c3b32b0ae223f57f347711d160a
Simon Larsén <slarse@slar.se>
2025-01-29 21:51:44 +0100

    Update dbee sample
    Signed-off-by: Simon Larsén <slarse@slar.se>

--- scripts/set_dbee_connections_sample.sh
+++ scripts/set_dbee_connections_sample.sh
@@ -1,13 +1,23 @@
 # Define connections
 export DBEE_CONNECTIONS='[
     {
-        "name": "{{ exec `echo Hidden Database` }}",
-        "url": "postgres://{{ env \"PGUSER\" }}:{{ env `PGPASSWORD` }}@localhost:5432/{{ env `PGDATABASE` }}?sslmode=disable",
+        "name": "{{ env "PGDATABASE" }}",
+        "url": "postgres://{{ env "PGUSER" }}:{{ env "PGPASSWORD" }}@localhost:5432/{{ env "PGDATABASE" }}?sslmode=disable",
         "type": "postgres"
+    },
+    {
+        "name": "{{ env "MYSQL_DATABASE" }}",
+        "url": "mysql://{{ env "MYSQL_USER" }}:{{ env "MYSQL_PASSWORD" }}@localhost:3306/{{ env "MYSQL_DATABASE" }}",
+        "type": "mysql"
     }
 ]'
 
-# Export secrets
+# Export Postgres secrets
 export PGUSER="postgres"
-export PGPASSWORD="mysecretpassword"
-export PGDATABASE="mydb"
+export PGPASSWORD="postgres"
+export PGDATABASE="test"
+
+# Export MariaDB secrets
+export MYSQL_USER="root"
+export MYSQL_PASSWORD="mariadb"
+export MYSQL_DATABASE="test"
