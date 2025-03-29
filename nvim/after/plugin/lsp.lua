8f03a6444a2b70171a29b65f44c7f4ab9d84fba7
Simon Larsén <slarse@slar.se>
2025-03-29 22:05:17 +0100

    Fix some autocmd groups

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -64,10 +64,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			})
 
 			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+				group = vim.api.nvim_create_augroup("slarse-lsp-detach", { clear = true }),
 				callback = function(event2)
 					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+					vim.api.nvim_clear_autocmds({ group = "slarse-lsp-highlight", buffer = event2.buf })
 				end,
 			})
 		end
